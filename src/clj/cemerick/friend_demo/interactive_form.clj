e7ef8eb8bd0aafc5990a6ce9306bea7884ddde77
Luke Crawford <luke0x@gmail.com>
2013-11-04 11:46:38 -0800

    Simplify markup

--- src/clj/cemerick/friend_demo/interactive_form.clj
+++ src/clj/cemerick/friend_demo/interactive_form.clj
@@ -14,11 +14,8 @@
             [hiccup.element :as e]))
 
 (def login-form
-  [:div {:class "row"}
-   [:div {:class "columns small-12"}
-    [:h3 "Login"]
-    [:div {:class "row"}
-     [:form {:method "POST" :action "login" :class "columns small-4"}
+  [:div {:class "row"} [:div {:class "columns small-12"} [:div {:class "row"}
+    [:form {:method "POST" :action "/interactive-form/login" :class "columns small-4"}
       [:div "Username" [:input {:type "text" :name "username"}]]
       [:div "Password" [:input {:type "password" :name "password"}]]
       [:div [:input {:type "submit" :class "button" :value "Login"}]]]]]])
